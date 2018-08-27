080cd8b4fbb4ec89aad021d308c62416f9503004
tim-peterson <petersontimr@gmail.com>
2018-08-27 11:17:21 -0500

    adding more stats to fig. 2

--- figure2.R
+++ figure2.R
@@ -7,9 +7,25 @@ p2 <- ggplot(cancer_vs_other, aes(x = `cancer`, y = `other`)) + geom_point(color
 
 # statistics
 
+# intercept and slope
+
 lm(formula = log(cancer_vs_other$other) ~ log(cancer_vs_other$cancer), 
     data = cancer_vs_other)
 
 #Coefficients:
 #                (Intercept)  log(cancer_vs_other$cancer)  
 #                     0.5281                       0.7532  
+
+
+# correlation coefficient 
+
+cor(log(cancer_vs_other$other),log(cancer_vs_other$cancer))
+
+#[1] 0.6890797
+
+
+# skewness
+fit = lm(formula = log(cancer_vs_other$other) ~ log(cancer_vs_other$cancer), data = cancer_vs_other)
+
+# plot shows skewness decreases up to about 10^4 citations
+plot(log(cancer_vs_other$other),abs(fit$residuals))
